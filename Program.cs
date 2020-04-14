f1127df3ae4e17f3c2f5132f5a599806c857db10
unknown <aminmirzoev123@gmail.com>
2020-04-14 19:03:20 +0500

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conecting_with_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
