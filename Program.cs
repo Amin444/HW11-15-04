2401fb72f6563306bd261249bc4dbe34bf3115b7
unknown <aminmirzoev123@gmail.com>
2020-04-15 23:10:38 +0500

    Chages into select and doing insert

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace conecting_with_C_
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Введите: 1-'Insert', 3-' Select All', 4-'Select by id', 5-'Update', 6-'Delete'   ");
+           int count=int.Parse(Console.ReadLine());
+            const string constring = @"data sourse=localhost; initial catalog=Person; integrated security=true";
+            SqlConnection connetion = new SqlConnection(constring);
+            connetion.Open();
+            if (count==1)
+            {
+                Select();
+            }
         }
+            static void Select(SqlConnection conn)
+            {
+                conn.Open();
+                SqlCommand Sel=new SqlCommand();
+                sel=$"Select *from Person";
+                SqlDataReader reader=conn.ExecuteReader();
+                 while (reader.Read())
+            {
+                System.Console.WriteLine($"ID:{reader.GetValue(0)},Name:{reader.GetValue(1)},SurName:{reader.GetValue(2)},MiddleName:{reader.GetValue(3)},BirthDate:{reader.GetValue(4)}");
+            }
+            conn.Close();
+        }
+      
     }
-}
+
+ }
+         
+          
+
+
